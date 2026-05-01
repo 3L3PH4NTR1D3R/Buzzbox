e2d6c1e0c85a2a6a6e0e270647314b072c7cf6e2
Victoria <stargoof44@gmail.com>
2026-05-01 17:07:54 -0400

    Added mode selection functionality, added icon, and changed style file.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,17 @@ const disconnectButton = document.getElementById('disconnectBleButton');
 const bleStateContainer = document.getElementById('bleState');
 const buzzerController = document.getElementById('buzzerControlButton');
 const buzzerActiveContainer = document.getElementById('buzzerState');
+const mode1Button = document.getElementById('selectMode1');
+const mode2Button = document.getElementById('selectMode2');
+const mode3Button = document.getElementById('selectMode3');
 const modeStateContainer = document.getElementById('modeState');
 
 var deviceName ='BuzzBox';
 var bleService = '19b10000-e8f2-537e-4f6c-d104768a1214';
 var ledCharacteristic = '19b10002-e8f2-537e-4f6c-d104768a1214';
 var sensorCharacteristic= '19b10001-e8f2-537e-4f6c-d104768a1214';
+var modeCharacteristic = '9c2758bc-8cf5-4b18-a0fb-3d60fa523c18';
+var patternCharacteristic = 'c9108ecc-0dde-4694-9735-e084da2ba23d';
 
 var bleServer;
 var bleServiceFound;
@@ -41,11 +46,54 @@ if (bleServer && bleServer.connected) {
     // Throw an error if Bluetooth is not connected
     console.error("Bluetooth is not connected.");
     window.alert("Bluetooth is not connected.")
-}
-    
-    
+}      
     
 });
+
+mode1Button.addEventListener('click', ()=> {
+
+    if (bleServer && bleServer.connected) {
+
+    modeStateContainer.innerHTML = "Manual";
+    writeMode(1);
+
+} else {
+    // Throw an error if Bluetooth is not connected
+    console.error("Bluetooth is not connected.");
+    window.alert("Bluetooth is not connected.")
+}      
+    
+})
+
+mode2Button.addEventListener('click', ()=> {
+
+    if (bleServer && bleServer.connected) {
+    
+    modeStateContainer.innerHTML = "Pulse";
+    writeMode(2);
+
+} else {
+    // Throw an error if Bluetooth is not connected
+    console.error("Bluetooth is not connected.");
+    window.alert("Bluetooth is not connected.")
+}      
+    
+})
+
+mode3Button.addEventListener('click', ()=> {
+
+    if (bleServer && bleServer.connected) {
+
+    modeStateContainer.innerHTML = "Pattern";
+    writeMode(3);
+
+} else {
+    // Throw an error if Bluetooth is not connected
+    console.error("Bluetooth is not connected.");
+    window.alert("Bluetooth is not connected.")
+}      
+    
+})
 
 function isWebBluetoothEnabled() {
     if (!navigator.bluetooth) {
@@ -104,6 +152,24 @@ function writeOnCharacteristic(value){
     } else {
             console.error ("Bluetooth is not connected. Cannot activate.")
             window.alert("Bluetooth is not connected. Cannot activate. \n Connect to Bluetooth first!")
+    }
+}
+
+function writeMode(value){
+    if (bleServer && bleServer.connected) {
+        bleServiceFound.getCharacteristic(modeCharacteristic)
+        .then(characteristic => {
+            console.log("Mode: "+ value);
+            console.log("Found the Mode characteristic: ", characteristic.uuid);
+            const data = new Uint8Array([value]);
+            return characteristic.writeValue(data);
+        })
+        .catch(error => {
+            console.error("Error writing to the Mode characteristic: ", error);
+        });
+    } else {
+            console.error ("Bluetooth is not connected. Cannot change.")
+            window.alert("Bluetooth is not connected. Cannot change. \n Connect to Bluetooth first!")
     }
 }
 
